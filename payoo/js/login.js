8f1b096addbc391d2836fbbb39e91e17753ac147
md ismail <mdismail85773@gmail.com>
2025-09-07 02:06:16 +0800

    logig validation cheak done

--- payoo/js/login.js
+++ payoo/js/login.js
@@ -3,10 +3,18 @@
 document.getElementById('loginBtn').addEventListener('click', function(e){
     // prevent defualt behavior (prevent loding browser)
     e.preventDefault();
-    console.log("login btn worked");
 
     // get phone number
     const mobileNumber = document.getElementById('mobile-number').value;
-        console.log(mobileNumber);
+
+    // get pin number
+    const pinNumber = document.getElementById('pin').value;
+
+    // this is temporary logic for  test 
+    if (mobileNumber ==='11' && pinNumber ==='1234'){
+        window.location.href ="/payoo/home.html"
+    }else{
+        console.log("Wrong Mobile Number or Pin ")
+    }
     
 })
