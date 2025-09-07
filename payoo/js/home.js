96d92477c2c6bb97b4e303453d54ae2c4c31ac1d
md ismail <mdismail85773@gmail.com>
2025-09-07 16:07:24 +0800

    cash out function done

--- payoo/js/home.js
+++ payoo/js/home.js
@@ -1,4 +1,4 @@
-// submit button function added
+// Add Mony Forms handle
 
 document.getElementById('btn-addmony').addEventListener('click', function(e){
     e.preventDefault();
@@ -23,6 +23,43 @@ document.getElementById('btn-addmony').addEventListener('click', function(e){
 
     }else{
         console.log("wrong pin")
+    }
+})
+
+
+
+// Cash Out Froms handle here
+    // cash out button hanndle function
+document.getElementById('btn-cashout').addEventListener('click',function(e){
+    e.preventDefault();
+        // console.log('Cash Out Button worked');
+    
+    // get amount function and conver string ot float 
+    const cashOutAmount = parseFloat(document.getElementById('cashout-amount').value);
+        // console.log(cashOutAmount);
+
+    // get pin function 
+    const cashOutPin = document.getElementById('cashout-pin').value;
+        // console.log(cashOutPin);
+
+    // get current balance function and convert string to float
+    const currentBalance = parseFloat(document.getElementById('current-balance').innerText);
+        // console.log(currentBalance);
+
+    // pin validatin function
+    if (cashOutPin ==='1234' && cashOutAmount <= currentBalance){
+            // console.log('Pin and Balance is valid');
+
+            // calculation new curren balancd after cash out an nad push the UI
+            const newCurrentBalance = currentBalance - cashOutAmount;
+                // console.log(newCurrentBalance);
+            document.getElementById('current-balance').innerText=newCurrentBalance;
+
+
+
+
+    }else{
+        console.log('Ivalid Pin'); 
     }
 
 
