2a3b8b3e7c996275c7f994966b407228c576ef45
md ismail <mdismail85773@gmail.com>
2025-09-07 13:32:29 +0800

    mony added validation check

--- payoo/js/home.js
+++ payoo/js/home.js
@@ -1,1 +1,25 @@
-console.log('home.js add')
+// submit button function added
+
+document.getElementById('btn-addmony').addEventListener('click', function(e){
+    e.preventDefault();
+    console.log("Submit workd")
+
+    // get amount
+    const amout = document.getElementById('amount').value;
+    console.log(amout); 
+
+    // get pin from home
+    const pin = document.getElementById('pin').value;
+    console.log(pin);
+
+    // pin validation
+    if (pin ==="4"){
+        console.log("mony add successful");
+        
+
+    }else{
+        console.log("wrong pin")
+    }
+
+
+})
